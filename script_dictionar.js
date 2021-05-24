c728732d0c2541ec9a6d3573c3a36145d671dcdb
mitiiuli <84138856+mitiiuli@users.noreply.github.com>
2021-05-24 16:54:38 +0300

    Update script_dictionar.js

--- script_dictionar.js
+++ script_dictionar.js
@@ -1,6 +1,5 @@
-
-function addword()
-{
+var dict = [];
+function addword() {
     var newWord = document.getElementById('newWord').value;
     console.log(newWord);
     if (!dict.includes(newWord)) {
@@ -15,15 +14,12 @@ function s_word(word) {
   return word == document.getElementById("search_word").value;
 }
 
-function searchword()
-{
-    document.getElementById("demo").innerHTML = dict.find(s_word);
-    if(dict.find(s_word))
-    {
-       document.getElementById("demo").innerHTML = "Cuvantul exista";
+function searchword() {
+    document.getElementById("status").innerHTML = dict.find(s_word);
+    if(dict.find(s_word)) {
+       document.getElementById("status").innerHTML = "Cuvantul exista";
     }
-    else
-    {
-       document.getElementById("demo").innerHTML = "Cuvantul nu exista";
+    else{
+       document.getElementById("status").innerHTML = "Cuvantul nu exista";
     }
 }
